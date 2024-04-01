db689b93423de7c0bfe6bec38e2bad3ec0681be9
jamesirungume <jymohcooler@gmail.com>
2024-04-01 18:47:46 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Homepage from './components/Homepage.jsx'
+import Navbar from './components/Navbar.jsx'
+
+
+
+export default function App() {
+  return (
+    <>
+    <Homepage />
+    <Navbar />
+    </>
+  )
+}
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+
+export default function Homepage() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
